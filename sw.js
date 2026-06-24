9b63641c031f463e72ea5712ef29fe3288c52c60
skksong-alt <skksong@gmail.com>
2026-06-24 15:58:19 +0400

    최종수정

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'bareaplay-cache-v46';
+const CACHE_NAME = 'bareaplay-cache-v47';
 const urlsToCache = [
     '/', '/index.html', '/css/style.css',
     '/js/app.js?v=5',
